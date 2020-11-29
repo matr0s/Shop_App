8330cd3f08b994898c9a9c3a06e968d6a1b3b430
matr0s <serhiy.osadchuk@gmail.com>
2020-11-29 17:54:29 +0200

    Working with Providers & Listeners

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/screens/product_details_screen.dart';
+import 'package:provider/provider.dart';
+
+import './screens/product_details_screen.dart';
 import './screens/products_overview_screen.dart';
+import './providers/products_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ElectroMag',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'ElectroMag',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        //home: ProductsOverviewScreen(),
+        initialRoute: ProductsOverviewScreen.routeId,
+        routes: {
+          ProductsOverviewScreen.routeId: (context) => ProductsOverviewScreen(),
+          ProductDetailsScreen.routeId: (context) => ProductDetailsScreen(),
+        },
       ),
-      //home: ProductsOverviewScreen(),
-      initialRoute: ProductsOverviewScreen.routeId,
-      routes: {
-        ProductsOverviewScreen.routeId : (context) => ProductsOverviewScreen(),
-        ProductDetailsScreen.routeId : (context) => ProductDetailsScreen(),
-      },
     );
   }
 }
---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
+import 'product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<ProductsProvider>(context);
+    final products = productsData.items;
+    return GridView.builder(
+      padding: EdgeInsets.all(10.0),
+      itemCount: products.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        childAspectRatio: 3 / 2,
+        crossAxisCount: 2,
+        mainAxisSpacing: 10.0,
+        crossAxisSpacing: 10.0,
+      ),
+      itemBuilder: (ctx, i) => ProductItem(
+        products[i].id,
+        products[i].title,
+        products[i].imageUrl,
+      ),
+    );
+  }
+}
