7bc96ae9109e95ae83d530e766f70027d31dba4a
matr0s <serhiy.osadchuk@gmail.com>
2020-11-29 18:47:35 +0200

    Provider updates for the Product details screen

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -43,6 +43,10 @@ class ProductsProvider with ChangeNotifier {
     return UnmodifiableListView(_items);
   }
 
+  Product findById(String id) {
+    return _items.firstWhere((prod) => prod.id == id);
+  }
+
   void addProduct() {
    // _items.add(value);
     notifyListeners();
--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import '../providers/products_provider.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   static const routeId = '/product-details';
-
   @override
   Widget build(BuildContext context) {
+//
+    // Getting arguments from NamedRoute
     final String productId =
         ModalRoute.of(context).settings.arguments as String;
-// Here wi are going to get all the data for my product based on ID
+//
+    // Here we are going to get all the data for my product based on ID
+    final loadedProduct = Provider.of<ProductsProvider>(
+      context,
+      listen: false,
+    ).findById(productId);
+// ======================================================
     return Scaffold(
       appBar: AppBar(
-        title: Text(productId),
+        title: Text(loadedProduct.title),
       ),
     );
   }
