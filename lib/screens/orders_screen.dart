7c38cc626cd2f568b411673bb9aebb725285a4c7
matr0s <serhiy.osadchuk@gmail.com>
2020-12-08 19:33:01 +0200

    Adding an Orders Screen

---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/orders.dart' show Orders;
+import '../widgets/order_item.dart';
+
+class OrdersScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final orders = Provider.of<Orders>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Orders'),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, i) => OrderItem(orders.orders[i]),
+        itemCount: orders.orders.length,
+      ),
+    );
+  }
+}
